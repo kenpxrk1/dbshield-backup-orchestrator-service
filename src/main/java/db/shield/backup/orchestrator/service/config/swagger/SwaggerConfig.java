018042d2045b18ef8811b40bb9eda07d7808bfdf
Dmitry Grishin <kenpxrkasf@gmail.com>
2026-04-12 19:44:03 +0300

    dev: modified swagger ui configuration

--- src/main/java/db/shield/backup/orchestrator/service/config/swagger/SwaggerConfig.java
+++ src/main/java/db/shield/backup/orchestrator/service/config/swagger/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package db.shield.backup.orchestrator.service.config.swagger;
 
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +19,14 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Configuration-Service")
                         .description("Service allows manage database configurations")
-                );
+                )
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
     }
 }
