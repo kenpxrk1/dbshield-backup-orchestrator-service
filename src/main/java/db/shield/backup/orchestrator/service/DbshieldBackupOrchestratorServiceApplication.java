80816a637c0f8fa2f559ad6d4802d14c3136a782
Dmitry Grishin <kenpxrkasf@gmail.com>
2026-04-12 19:42:03 +0300

    dev: add auth-lib

--- src/main/java/db/shield/backup/orchestrator/service/DbshieldBackupOrchestratorServiceApplication.java
+++ src/main/java/db/shield/backup/orchestrator/service/DbshieldBackupOrchestratorServiceApplication.java
@@ -2,8 +2,12 @@ package db.shield.backup.orchestrator.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "db.shield")
+@EnableJpaAuditing
 public class DbshieldBackupOrchestratorServiceApplication {
 
     public static void main(String[] args) {
